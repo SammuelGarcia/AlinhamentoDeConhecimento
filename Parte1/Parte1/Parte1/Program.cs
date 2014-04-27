fbacbd0ce5f0922baf4798492c595fdf55072ba7
SammuelGarcia <sammuel_garcia@hotmail.com>
2014-04-26 22:37:34 -0700

    Parte 1

---
+++ Parte1/Parte1/Parte1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte1
+{
+    class ParteUm
+    {
+        static void Main(string[] args)
+        {
+            //Tipo/ Nome/ recebe /  valor
+            string NumeroString1 = "1";
+            string NumeroString2 = "1";
+            
+            int numero1 = 1;
+            int numero2 = 1;
+
+            //Exibe no console
+            Console.WriteLine(numero1 + numero2);
+            Console.WriteLine(NumeroString1 + NumeroString2);
+
+            
+            //   variavel        Condicao     incremento +1 a variavel
+            for (int numero = 0; numero < 10; numero++)
+            {
+                Console.WriteLine(numero1);
+                Console.WriteLine(numero);
+            }
+
+            while (numero1 == 1)
+            {
+                Console.WriteLine("Igual");
+            }
+        }
+    }
+}
--- Parte1/Parte1/Parte1/Properties/AssemblyInfo.cs
+++ Parte1/Parte1/Parte1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aula1")]
+[assembly: AssemblyTitle("Parte1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aula1")]
+[assembly: AssemblyProduct("Parte1")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a6296744-a30d-4534-ac5e-97ccae732bb1")]
+[assembly: Guid("e9bba30a-1328-425c-90db-510211ffac7d")]
 
 // Version information for an assembly consists of the following four values:
 //
